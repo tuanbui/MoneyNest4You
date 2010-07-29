9469bd01ce889583c4671241f519eadbd3e5a5f3
tuanbui <tuan_b918@yahoo.com>
2010-07-28 17:40:12 -0700

    Create frame and empty table

--- main.java
+++ main.java
@@ -1,13 +1,41 @@
 import javax.swing.*;
+
+import java.awt.Dimension;
+import java.awt.GridLayout;
 import java.awt.event.*;
 
-public class main {
+public class main extends JPanel{
+	public main() {
+        super(new GridLayout(1,0));
+
+        String[] columnTitles = {"First Name",
+                                "Last Name",
+                                "Account Type",
+                                "Account Number",
+                                "Transaction Amount"};
+
+        Object[][] data = {
+	    {"-", "-",
+	     "-", "00000000", new Float(0.0)}
+        };
+        
+        
+
+        JTable table = new JTable(data, columnTitles);
+        table.setSize(1100, 200);
+
+        JScrollPane scroll = new JScrollPane(table);
+        add(scroll);
+    }
 
 	public static void main (String[] args){
 		JFrame frame = new JFrame("MoneyNest4You");
 		frame.setVisible(true);
-		frame.setSize(840,500);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(650, 300);
+		main newContentPane = new main();
+        newContentPane.setOpaque(true); 
+        frame.setContentPane(newContentPane);
 		
 		JMenuBar menubar = new JMenuBar();
 		frame.setJMenuBar(menubar);
