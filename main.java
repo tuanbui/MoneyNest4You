5ee9b770056c82e5bdb152c083ed7c7c92f9bd7a
tuanbui <tuan_b918@yahoo.com>
2010-07-28 20:33:39 -0700

    Fixed table title bug.

--- main.java
+++ main.java
@@ -1,43 +1,44 @@
 import javax.swing.*;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.*;
 
 public class main extends JPanel{
-	public main() {
-        super(new GridLayout(1,0));
-
-        String[] columnTitles = {"First Name",
-                                "Last Name",
-                                "Account Type",
-                                "Account Number",
-                                "Transaction Amount"};
-
-        Object[][] data = {
-	    {"-", "-",
-	     "-", "00000000", new Float(0.0)}
-        };
-        
-        
-
-        JTable table = new JTable(data, columnTitles);
-        table.setSize(1100, 200);
-
-        JScrollPane scroll = new JScrollPane(table);
-        add(scroll);
-    }
-
+	
 	public static void main (String[] args){
+		String[] columnTitles = {"First Name",
+	            "Last Name",
+	            "Account Type",
+	            "Account Number",
+	            "Transaction Amount"};
+
+	Object[][] data = {
+	{"-", "-",
+	"-", "00000000", new Float(0.0)},
+	{"-", "-",
+	"-", "00000000", new Float(0.0)},
+	{"-", "-",
+	"-", "00000000", new Float(0.0)},
+	{"-", "-",
+	"-", "00000000", new Float(0.0)},
+	{"-", "-",
+	"-", "00000000", new Float(0.0)},
+	{"-", "-",
+	"-", "00000000", new Float(0.0)},
+	{"-", "-",
+	"-", "00000000", new Float(0.0)},
+	{"-", "-",
+	"-", "00000000", new Float(0.0)},
+	{"Tuan", "-",
+		"-", "00000000", new Float(0.0)}
+		};
 		JFrame frame = new JFrame("MoneyNest4You");
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(650, 300);
-		main newContentPane = new main();
-        newContentPane.setOpaque(true); 
-        frame.setContentPane(newContentPane);
-		
-		JMenuBar menubar = new JMenuBar();
+		frame.setSize(900, 300);
+        JMenuBar menubar = new JMenuBar();
 		frame.setJMenuBar(menubar);
 		
 		JMenu file = new JMenu("File");
@@ -50,11 +51,18 @@ public class main extends JPanel{
 		JMenuItem about = new JMenuItem("About");
 		help.add(about);
 		
+		JTable table = new JTable(data, columnTitles);
+        frame.add(table);
+        
+        JScrollPane scroll = new JScrollPane(table);
+        frame.add(scroll);
+		
 		class exitaction implements ActionListener{
 			public void actionPerformed (ActionEvent e) {
 				System.exit(0);
 			}
 		}
+		
 		exit.addActionListener(new exitaction());
 	}
 }
